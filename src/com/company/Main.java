c399cd46e22e8cfef74b62d98d7941385c0bc7ba
Pedrohhenrique <pedrohdosantosss@gmail.com>
2022-08-02 19:56:47 -0300

    Meu primeiro commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	funsaoSoma(2,2);
+    }
+    public static void funsaoSoma(int x, int y){
+        int z = x+y;
+        System.out.println("a soma de" + x +"mais"+y+"é igual a"+z);
+    }
+}
