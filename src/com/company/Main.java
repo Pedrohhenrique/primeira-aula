a78d427df52b165e044978a40a3e515160821549
Pedrohhenrique <pedrohdosantosss@gmail.com>
2022-08-02 21:04:46 -0300

    segunda função

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	funsaoSoma(2,2);
+    funsaosubtrair(2,5);
+    }
+    public static void funsaosubtrair(double x, double y){
+        double z = x -y;
     }
     public static void funsaoSoma(int x, int y){
         int z = x+y;
